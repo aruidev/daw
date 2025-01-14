d3f9f35315cd7ce8d45bc7f5d36a2cf0bf4321d2
Alex Ruiz <arucadev@gmail.com>
2025-01-14 19:08:08 +0100

    Delete programming/java/TreasureQuest directory

--- programming/java/TreasureQuest/out/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"CercaDelTresor"},{"p":"controlador","l":"Controlador"},{"p":"llibreria","l":"Llibreria"},{"p":"model","l":"Model"},{"p":"vista","l":"Vista"}];updateSearchResults();
